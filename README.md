f7784f1c413c70e78d9daeb213e2bb6a64ba4cba
piaoger <piaoger@yahoo.com>
2016-04-23 19:23:37 +0800

    add playground golang

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# playground-golang
+
+# Playground of golang
+
+
+There are some materials for learning golang. It's the my start point striving for go!!
+
+## What's in?
+
++ Documents
+
+    My note of Go language. It's also my "The first glance at go".
+
++ playground
+
+    Source code
+
+
+## Examples
+
+All my examples are placed in PlayGround folder.
+
+For each example, there are at least 2 *.bat file to set up environment and build.
+
++ firsttour
+
+    Two basic examples: hello world and basic data types
+
++ actor
+
++ concurrency
+
+    An example about how to use goroutine and channel
+
++ mapreduce
+
+    An example about how to develop MapReduce with GO language
+
++ websocket
+
+    An example about how to create a web application with websocket.
+
++ appengine
+
+    An example about how to create go application on Google App Engine
+
++ httpget
+
+    An example about how to use net.http module.
---
+++ playground/readjson/readjson.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "encoding/json"
+    "os"
+    "log"
+    "fmt"
+)
+
+func main() {
+
+    // Read Configuration
+    fd, err := os.OpenFile("config.json", os.O_RDONLY | os.O_CREATE, 0)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer fd.Close()
+
+    // All member should be in upper case??? Yes, that's the secret!!
+    type Bazzar struct {
+        Bazzar_Name, Bazzar_Api_Host, Bazzar_Host, Bazzar_Summary string
+    }
+    type Configuration struct {
+        Bazzar Bazzar
+    }
+
+    var configuration Configuration
+    decoder := json.NewDecoder(fd)
+    for {
+        if err := decoder.Decode(&configuration); err != nil{
+            break
+        }
+       fmt.Printf("Summary: %s\n", configuration.Bazzar.Bazzar_Summary)
+    }
+
+    fd, err = os.OpenFile("config.rewrite.json", os.O_RDONLY | os.O_CREATE, 0)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer fd.Close()
+
+    encoder := json.NewEncoder(fd)
+    if err := encoder.Encode(configuration); err != nil {
+        fmt.Println("Cannot encode:", err)
+    }
+
+    // There is another open sourced project. Looks good :
+    // https://github.com/bemasher/JSONGen
+
+    // https://github.com/aawc/learn/blob/master/Go/GoTour/src/code.google.com/p/go.talks/2012/zen/jsonformat.go
+
+}
